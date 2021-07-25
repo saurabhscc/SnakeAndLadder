aa05f44f52174910f53b8386c82a77042bc54dbe
saurabhs <saurabhpshirsath@gmail.com>
2021-07-25 22:38:45 +0530

    [saurabh] Add; Check For Player Roll The Dice

--- SnakeLadderStimulation.cs
+++ SnakeLadderStimulation.cs
@@ -6,10 +6,23 @@ namespace SnakeAndLadder
 {
     class SnakeLadderStimulation
     {
+        /// <summary>
+        /// This Method is For Random Dice Value
+        /// </summary>
       public void Snake()
         {
             int position = 0;
-            Console.WriteLine("Player is at position : " + position);
+            Console.WriteLine("Player is at position: " + position);
+            Random random = new Random();
+            int droll = 0;
+            while (true)
+            {
+                droll = random.Next(7);
+                if (droll != 0) break;
+
+            }
+            Console.WriteLine(droll);
         }
     }
-}
+    }
+
